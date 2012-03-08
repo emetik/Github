b8aa67491fffcfea717d6c0f23eadf92a6bea4eb
emetik <emetik@gmail.com>
2012-03-08 11:39:57 +0100

    test

--- index.php
+++ index.php
@@ -9,4 +9,30 @@ echo "encore mieux que lala et encore";
 echo "encore mieux que lala et encore et encore plus";
 
 
+echo "encore mieux que lala et encore et encore plus";
+
+echo "encore mieux que lala et encore et encore plus";
+echo "encore mieux que lala et encore et encore plus";
+echo "encore mieux que lala et encore et encore plus";
+echo "encore mieux que lala et encore et encore plus";
+echo "encore mieux que lala et encore et encore plus";
+echo "encore mieux que lala et encore et encore plus";
+echo "encore mieux que lala et encore et encore plus";
+echo "encore mieux que lala et encore et encore plus";
+
+
+$TAB	=	array('lala','toto');	
+$l		=	count($TAB);
+$o		=	'';
+for($i=0;$i<$l;$i++){
+
+	$o	.=	$TAB[$i];
+
+
+
+}
+echo $o;
+
+echo "lala,lklk ghgghg";
+
 ?>
